14eb4dd02b8a3d954f9e295bbaa92d19fc394630
sameersawale <samabhi519@gmail.com>
2026-07-15 20:14:46 +0530

    added swageer api
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/example/TicketTrove/Config/SecurityConfig.java

---
+++ src/main/java/com/example/TicketTrove/Config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.example.TicketTrove.Config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Auth Service API",
+                version = "v1",
+                description = "Authentication APIs for Smart Irrigation System"
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
